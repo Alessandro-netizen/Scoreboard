3f0613ab44b84ab2477f1ddac1738dac7a9502bf
Alessandro-netizen <alessandrom12@nycstudents.net>
2024-10-16 12:09:15 -0400

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -3,53 +3,33 @@ public class Scoreboard
     private String teamOne;
     private String teamTwo;
     private int teamOneScore;
-        private String activeTeam;
-            
     private int teamTwoScore;
+    private String activeTeam;
+    public String getScore()
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
     public Scoreboard(String one, String two)
     {
         teamOne = one;
-            teamTwo = two;
-    }
-
-
-
-
-
-
-        public String getScore()
-        {
-            return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
-        }
-
-        
-    public void recordPlay( int score) 
-    {
-        
-        if(score == 0)
-        {
-            if(activeTeam.equals(teamOne)) 
-                activeTeam = teamTwo;
-                    else if(activeTeam.equals(teamTwo)) 
-                        activeTeam = teamOne;
-        }
-        else {
-                            if(teamOne.equals(activeTeam)) 
-                                teamOneScore += score;
-                                    if(teamTwo.equals(activeTeam)) 
-                                        teamTwoScore += score;
-
-        }
-            
-        
-    
-
-        
-
+        teamTwo = two;
+        activeTeam = teamOne;
 
     }
-    
-    
-   
+    public void recordPlay(int score)
+    { if(score == 0)
+        {
+            if(activeTeam.equals(teamOne)) activeTeam = teamTwo;
+            else if (activeTeam.equals(teamTwo)) activeTeam = teamOne;
+        }
+
+        if(teamOne.equals(activeTeam)){
+            teamOneScore += score;
+        }
+        if(teamTwo.equals(activeTeam)){
+            teamTwoScore += score;
+        }
+
+    }
 
 }
